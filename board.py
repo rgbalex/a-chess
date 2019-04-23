bb05316e35ec204cd217e7677f184ec6fb4d7d64
Alex Angelidis <alex.m.angelidis@gmail.com>
2019-04-23 14:43:29 +0100

    Ensured _init_ works correctly passing classes not module namespaces

--- board.py
+++ board.py
@@ -5,3 +5,15 @@ class Board(object):
     def __init__(self):
         super(Board, self).__init__()
         self.chessBoard = [ ["" for j in range(8)] for i in range(8) ]
+
+    def __str__(self):
+        for i in self.chessBoard:
+            print(i)
+        return ""
+
+    def place(self,obj,place):
+        x = place[0]-1
+        y = place[1]-a
+        ret = self.chessBoard[y][x]
+        self.chessBoard[y][x] = obj
+        return ret
--- pieces/__init__.py
+++ pieces/__init__.py
@@ -1,12 +1,22 @@
 # With inspiration from answers to code from StackOverflow
 # https://stackoverflow.com/questions/1057431/how-to-load-all-modules-in-a-folder
 
-__all__ = []
+# __all__ = []
+#
+# import os
+# for module in os.listdir(os.path.dirname(__file__)):
+#     if module in ('__init__.py', 'piece.py') or module[-3:] != '.py':
+#         continue
+#     __all__.append(module[:-3])
+# print(__all__)
+# del module
+# del os
 
-import os
-for module in os.listdir(os.path.dirname(__file__)):
-    if module in ('__init__.py', 'piece.py') or module[-3:] != '.py':
-        continue
-    __all__.append(module[:-3])
-del module
-del os
+# Was only importing module namespace and not the classes.
+
+from .bishop import Bishop
+from.king import King
+from .knight import Knight
+from .pawn import Pawn
+from.queen import Queen
+from .rook import Rook
